15a167e8f408c4c85a5ed215d12f2be267ccec53
MartinXiBerg <martin.deutschmann@outlook.de>
2024-11-05 16:52:04 +0100

    dayend commit

---
+++ Lab1/AspireExample/Contracts/IBeerService.cs
@@ -0,0 +1,13 @@
+﻿using BeerStore.Models;
+
+namespace BeerStore.Contracts
+{
+  public interface IBeerService
+  {
+    void AddBeer(Beer beer);
+    Beer? GetBeer(int id);
+    IEnumerable<Beer> GetBeers();
+    void RemoveBeer(int id);
+    void UpdateBeer(int id, Beer beer);
+  }
+}
